68b471fa481064618ca0fba3c49242f0f6424ae3
sulemanfarooqi <suleman.farooqi.sf@gmail.com>
2021-02-03 20:59:09 -0600

    Initial commit

---
+++ src/main/java/com/sulemanfarooqi/z30registry/Z30registryApplication.java
@@ -0,0 +1,13 @@
+package com.sulemanfarooqi.z30registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Z30registryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Z30registryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sulemanfarooqi/z30registry/Z30registryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sulemanfarooqi.z30registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Z30registryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
